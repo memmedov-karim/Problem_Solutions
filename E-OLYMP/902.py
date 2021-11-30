53fcef4cf521deb41a300e6467df7943cdd5b52c
memmedov-karim <71795994+memmedov-karim@users.noreply.github.com>
2021-11-30 19:44:15 +0400

    Create 902.py

---
+++ E-OLYMP/902.py
@@ -0,0 +1,9 @@
+n=int(input())
+if n>=1 and n<=3:
+    print("Initial")
+elif n>=4 and n<=6:
+    print("Average")
+elif n>=7 and n<=9:
+    print("Sufficient")
+elif n>=10 and n<=12:
+    print("High")
