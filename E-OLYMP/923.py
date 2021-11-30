69771e752928d7c2eb6bfc0c9768c5fb78299b69
memmedov-karim <71795994+memmedov-karim@users.noreply.github.com>
2021-11-30 20:10:12 +0400

    Create 923.py

---
+++ E-OLYMP/923.py
@@ -0,0 +1,9 @@
+n=int(input())
+if (n==1 or n==2 or n==12):
+    print("Winter")
+elif (n>2 and n<6):
+    print("Spring")
+elif (n>5 and n<9):
+    print("Summer")
+elif (n==9 or n==10 or n==11):
+    print("Autumn")
